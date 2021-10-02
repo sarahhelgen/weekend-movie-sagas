c91dc628691536c63be34d275cbf4462b26b5123
Sarah Helgen <sarah.helgen@gmail.com>
2021-10-02 06:56:22 -0500

    fetch movie details dispatched from movielist to index.js, fetch movie details saga triggered

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,28 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom'
 import './MovieList.css'
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    const history = useHistory();
 
+    //on page load, fetch movie list from server/db. this dispatch will trigger
+    // the fetchallmovies saga, which will pull the data from the db and then trigger
+    //the movies reducer to display the data on the DOM.
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    const viewSelectedMovie = () => {
+        console.log('in viewSelectedMovie'); //click handler is working
+       
+        dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: movies.id });
+        //payload is the individual movie, which comes from the map below.
+        history.push('/details');
+    }
 
     return (
         <main>
@@ -17,9 +30,9 @@ function MovieList() {
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
+                        <div key={movie.id} onClick={viewSelectedMovie}>
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} />
                         </div>
                     );
                 })}
--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ import axios from 'axios';
 // Create the rootSaga generator function
 function* rootSaga() {
     yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+    yield takeEvery('FETCH_MOVIE_DETAILS', fetchMovieDetails );
+}
+
+function* fetchMovieDetails(){
+    console.log('fetchMovieDetails saga triggered');
 }
 
 function* fetchAllMovies() {
